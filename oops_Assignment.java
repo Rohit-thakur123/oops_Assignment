b31679fcb1b024fe4bbdfe66460b19ff81f8b608
Rohit Singh <thakurrohitsingh.up85@gmail.com>
2025-08-27 08:49:58 +0530

    2_multiplication table

--- oops_Assignment.java
+++ oops_Assignment.java
@@ -7,12 +7,25 @@ public class oops_Assignment {
         return sum;
     }
 
+// ans 2: multiplication table
+    public static void table(int n){
+        for(int i=1; i<=10; i++){
+            System.out.println(n+" X "+i+" = "+(n*i));
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 // ques 1: Sum of Natural Numbers
-        System.out.print("enter the no: ");
+        // System.out.print("enter the no: ");
+        // int n=sc.nextInt();
+        // System.out.println("hence the sum of first "+n+" natural no is: "+sum_natural_no(n));
+
+// ques 2: Multiplication Table
+        System.out.print("enter the table no: ");
         int n=sc.nextInt();
-        System.out.println("hence the sum of first "+n+" natural no is: "+sum_natural_no(n));
+        table(n);
+
     }
-    
+
 }
